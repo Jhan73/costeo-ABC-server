92f2cce723f3debf381568828672d0117a6e2e4c
YvesZavaletaB <yveszavaletab@gmail.com>
2023-11-17 14:41:40 -0500

    Modelos creados. Solo falta las llaves compuestas.

--- apps/maintenance/models.py
+++ apps/maintenance/models.py
@@ -1,17 +1,4 @@
 from django.db import models
-    
-class Activity (models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    cost = models.DecimalField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_user = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-    updated_user = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
 
 class ActivityCenter (models.Model):
     id = models.AutoField(primary_key=True)
@@ -24,12 +11,6 @@ class ActivityCenter (models.Model):
 
     def __str__(self):
         return self.name
-    
-class ActivityCostElement (models.Model):
-    cantidad = models.IntegerField()
-
-    def __str__(self):
-        return self.cant
 
 class CostElement (models.Model):
     id = models.AutoField(primary_key=True)
@@ -64,12 +45,6 @@ class CostObject (models.Model):
     def __str__(self):
         return self.name
 
-class CostObjectInvoice (models.Model):
-    cant = models.IntegerField()
-
-    def __str__(self):
-        return self.cant
-
 class Customer (models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -97,12 +72,6 @@ class DirectLabor (models.Model):
     def __str__(self):
         return self.cant
 
-class DirectLaborCostObject (models.Model):
-    cant = models.IntegerField()
-
-    def __str__(self):
-        return self.cant
-
 class DirectRawMaterial (models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
@@ -117,12 +86,6 @@ class DirectRawMaterial (models.Model):
 
     def __str__(self):
         return self.name
-
-class DirectRawMaterialCostObject1 (models.Model):
-    cant = models.IntegerField()
-
-    def __str__(self):
-        return self.cant
 
 class DirectService (models.Model):
     id = models.AutoField(primary_key=True)
@@ -151,6 +114,7 @@ class Employee (models.Model):
     created_user = models.CharField(max_length=255)
     updated_at = models.DateTimeField(auto_now=True)
     updated_user = models.CharField(max_length=255)
+    direct_labor_id = models.ForeignKey(DirectLabor)
 
     def __str__(self):
         return self.name
@@ -166,23 +130,10 @@ class Group (models.Model):
     def __str__(self):
         return self.name
 
-class Inductor(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    code = models.CharField(max_length=50)
-    description = models.TextField()
-    state = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    created_user = models.CharField(max_length=255)
-    updated_at = models.DateTimeField(auto_now=True)
-    updated_user = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
 class Invoice(models.Model):
     id = models.AutoField(primary_key=True)
     issue_date = models.DateTimeField(auto_now_add=True)
+    customer_id = models.ForeignKey(Customer)
     
     def __str__(self):
         return self.issue_date
@@ -191,6 +142,7 @@ class Payment(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
     total = models.DecimalField()
+    customer_id = models.ForeignKey(Customer)
 
     def __str__(self):
         return self.total
@@ -216,16 +168,12 @@ class Supplier (models.Model):
     created_user = models.CharField(max_length=255)
     updated_at = models.DateTimeField(auto_now=True)
     updated_user = models.CharField(max_length=255)
+    #################Nombre Raro
+    resource_id = models.ForeignKey(CostElement)
+    ############################
 
     def __str__(self):
         return self.name
-
-class Token (models.Model):
-    key = models.CharField(primary_key=True; max_length==255)
-    created = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.key
 
 class User (models.Model):
     id = models.AutoField(primary_key=True)
@@ -241,3 +189,125 @@ class User (models.Model):
 
     def __str__(self):
         return self.user_name
+    
+class Activity (models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    cost = models.DecimalField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_user = models.CharField(max_length=255)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_user = models.CharField(max_length=255)
+    activity_center_id = models.ForeignKey(ActivityCenter)
+
+    def __str__(self):
+        return self.name
+
+class Inductor(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=50)
+    description = models.TextField()
+    state = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_user = models.CharField(max_length=255)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_user = models.CharField(max_length=255)
+    inductor_id = models.ForeignKey(Activity)
+
+    def __str__(self):
+        return self.name
+
+class Token (models.Model):
+    key = models.CharField(primary_key=True; max_length==255)
+    created = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(User)
+    
+    def __str__(self):
+        return self.key
+
+
+
+
+
+class ActivityCostElement (models.Model):
+    activity_id = models.ForeignKey(Activity)
+    cost_element_id = models.ForeignKey(CostElement)
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return self.cant
+
+class ActivityCostObject (models.Model):
+    activity_id = models.ForeignKey(Activity)
+    cost_object_id = models.ForeignKey(CostObject)
+
+    def __str__(self):
+        return self.cant
+
+class CostObjectInvoice (models.Model):
+    invoice_id = models.ForeignKey(Invoice)
+    cost_object_id = models.ForeignKey(CostObject)
+    cant = models.IntegerField()
+
+    def __str__(self):
+        return self.cant
+
+class DirectLaborCostObject (models.Model):
+    direct_labor_id = models.ForeignKey(DirectLabor)
+    cost_object_id = models.ForeignKey(CostObject)
+    cant = models.IntegerField()
+
+    def __str__(self):
+        return self.cant
+
+class DirectRawMaterialCostObject1 (models.Model):
+    direct_raw_material_id = models.ForeignKey(DirectRawMaterial)
+    cost_object_id = models.ForeignKey(CostObject)
+    cant = models.IntegerField()
+
+    def __str__(self):
+        return self.cant
+
+class DirectsServiceCostObject (models.Model):
+    direct_service_id = models.ForeignKey(DirectService)
+    cost_object_id = models.ForeignKey(CostObject)
+    
+    def __str__(self):
+        return self.cant
+
+class GroupPermission (models.Model):
+    group_id = models.ForeignKey(Group)
+    permission_id = models.ForeignKey(Permission)
+
+    def __str__(self):
+        return self.cant
+
+class SupplierDirectRawMaterial (models.Model):
+    supplier_id = models.ForeignKey(Supplier)
+    direct_raw_material_id = models.ForeignKey(DirectRawMaterial)
+
+    def __str__(self):
+        return self.cant
+
+class SupplierDirectService (models.Model):
+    supplier_id = models.ForeignKey(Supplier)
+    direct_service_id = models.ForeignKey(DirectService)
+
+    def __str__(self):
+        return self.cant
+
+class UserGroup (models.Model):
+    user_id = models.ForeignKey(User)
+    group_id = models.ForeignKey(Group)
+
+    def __str__(self):
+        return self.cant
+
+class UserPermission (models.Model):
+    user_id = models.ForeignKey(User)
+    permission_id = models.ForeignKey(Permission)
+
+    def __str__(self):
+        return self.cant
